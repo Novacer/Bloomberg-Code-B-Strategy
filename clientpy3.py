9145ad0e665abbd89e883c9794d41ff8db03b694
Jackamac-attack <zhangjack27@gmail.com>
2018-03-16 22:45:08 -0400

    added manual control

--- clientpy3.py
+++ clientpy3.py
@@ -1,6 +1,32 @@
 import socket
 import sys
 from time import sleep
+import math
+
+
+from tkinter import *
+
+main = Tk()
+
+def leftKey(event):
+    move(3.1415926535)
+
+def rightKey(event):
+    move(0)
+
+def upKey(event):
+    move(-1.57)
+
+def downKey(event):
+    move(1.57)
+
+
+frame = Frame(main, width=100, height=100)
+main.bind('<Left>', leftKey)
+main.bind('<Right>', rightKey)
+main.bind('<Up>', upKey)
+main.bind('<Down>', downKey)
+frame.pack()
 
 mines = []
 
@@ -36,8 +62,30 @@ def move(angle):
     run(USER, PASSWORD, "ACCELERATE " + str(angle) + " 1")
 
 def target(our_x, our_y, our_dx, our_dy, x, y):
-    print(str(our_x) + " " + str(our_y))
-    print(str(x) + " " + str(y))
+    direction_to_face = [x - our_x, y - our_y]
+    current_velocity = [our_dx, our_dy]
+
+
+    norm = math.sqrt(direction_to_face[0] ** 2 + direction_to_face[1] ** 2)
+    if norm == 0 :
+        return
+
+    d_norm = [x/norm for x in direction_to_face]
+    v_norm_factor = math.sqrt(current_velocity[0]**2 + current_velocity[1] ** 2)
+    if v_norm_factor == 0:
+        return
+
+    v_norm = [x/v_norm_factor for x in current_velocity]
+
+    accel_vector = [(3 * v_norm[0]) - (3 * d_norm[0]), (3 * v_norm[1]) - (3 * d_norm[1])]
+    if accel_vector[0]:
+        return
+
+    angle = - math.atan(accel_vector[1] / accel_vector[0])
+
+    move(angle)
+
+    status()
 
 
 def status():
@@ -71,8 +119,6 @@ def handle_status(status):
 
         our_dy = float(our_coord[:space])
 
-
-
         index = status.find("MINES")
         if int(status[index+6:index+7]) > 0:
             relevant_status = status[index+8:]
@@ -92,12 +138,18 @@ def handle_status(status):
             if mine not in mines:
                 mines.append(mine)
 
-
             print(mines)
             target(our_x_coord, our_y_coord, our_dx, our_dy, coord_x, coord_y)
+        elif mines:
+            sleep(1)
+            coords = mines[-1]
+            target(our_x_coord, our_y_coord, our_dx, our_dy, coords[0], coords[1])
 
 move(3.14/6)
+
+main.mainloop()
 
 while(True):
     sleep(0.01)
     status()
+
