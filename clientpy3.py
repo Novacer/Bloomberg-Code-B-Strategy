887ae4907d654b87f34c42a6b1528686b466d77a
Jackamac-attack <zhangjack27@gmail.com>
2018-03-16 20:32:35 -0400

    handle status

--- clientpy3.py
+++ clientpy3.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+from time import sleep
+
 
 
 def run(user, password, * commands):
@@ -12,7 +14,7 @@ def run(user, password, * commands):
         sfile = sock.makefile()
         rline = sfile.readline()
         while rline:
-            print(rline.strip())
+            handle_status(rline.strip())
             rline = sfile.readline()
 
 def subscribe(user, password):
@@ -31,11 +33,22 @@ USER = "Friends might come"
 PASSWORD = "jzqzhang"
 
 def move(angle):
-    run(USER, PASSWORD, "ACCELERATE " + angle + " 1")
+    run(USER, PASSWORD, "ACCELERATE " + str(angle) + " 1")
 
 def status():
-    run(USER, PASSWORD, "STATUS")
+    return run(USER, PASSWORD, "STATUS")
 
 def brake():
     run(USER, PASSWORD, "BRAKE")
 
+def scan(x, y):
+    run(USER, PASSWORD, "SCAN " + str(x) + " " + str(y))
+
+def handle_status(status):
+    print(status[0:10])
+
+move(3.14/6)
+
+while(True):
+    sleep(1)
+    status()
